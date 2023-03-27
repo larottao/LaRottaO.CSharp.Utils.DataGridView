1e1c848e5957d9377e42314abec8b726c4085811
Luis Felipe La Rotta <larottao@gmail.com>
2023-03-27 16:25:16 -0600

    Deleted smelly return Task.Run(()

--- LaRottaO.CSharp.DataGridViewUtilities/ShowMultipleObjectsAsRows.cs
+++ LaRottaO.CSharp.DataGridViewUtilities/ShowMultipleObjectsAsRows.cs
@@ -16,128 +16,125 @@ namespace LaRottaO.CSharp.DataGridViewUtilities
 
         /*******************************************************************/
 
-        public Task<DataTable> show(List<object> argObjectList, Boolean argShowDebug = true)
+        public async Task<DataTable> show(List<object> argObjectList, Boolean argShowDebug = true)
         {
-            return Task.Run(() =>
+            try
             {
-                try
+                DataTable outputDataTable = new DataTable();
+
+                outputDataTable.Clear();
+                outputDataTable.Columns.Clear();
+
+                if (argObjectList.Count == 0)
                 {
-                    DataTable outputDataTable = new DataTable();
-
-                    outputDataTable.Clear();
-                    outputDataTable.Columns.Clear();
-
-                    if (argObjectList.Count == 0)
+                    if (argShowDebug)
                     {
-                        if (argShowDebug)
-                        {
-                            Console.WriteLine("There are no objects to show on the Datatable.");
-                        }
-                        return outputDataTable;
+                        Console.WriteLine("There are no objects to show on the Datatable.");
                     }
+                    return outputDataTable;
+                }
 
-                    Tuple<Boolean, String> columnCreationResult = DgvUtilsShared.createColumnsFromObjectProperties(argObjectList[0], outputDataTable);
+                Tuple<Boolean, String> columnCreationResult = DgvUtilsShared.createColumnsFromObjectProperties(argObjectList[0], outputDataTable);
 
-                    if (!columnCreationResult.Item1)
+                if (!columnCreationResult.Item1)
+                {
+                    Console.WriteLine("Unable to create the Datatable Columns: " + columnCreationResult.Item2);
+                }
+
+                PropertyInfo[] propertiesToRetrieveList = argObjectList[0].GetType().GetProperties();
+
+                foreach (Object objectOnList in argObjectList)
+                {
+                    List<String> listOfValuesForDataTableRow = new List<String>();
+
+                    foreach (PropertyInfo propertyToRetrieve in propertiesToRetrieveList)
                     {
-                        Console.WriteLine("Unable to create the Datatable Columns: " + columnCreationResult.Item2);
-                    }
-
-                    PropertyInfo[] propertiesToRetrieveList = argObjectList[0].GetType().GetProperties();
-
-                    foreach (Object objectOnList in argObjectList)
-                    {
-                        List<String> listOfValuesForDataTableRow = new List<String>();
-
-                        foreach (PropertyInfo propertyToRetrieve in propertiesToRetrieveList)
+                        try
                         {
-                            try
+                            /*******************************************************************/
+                            // If the property value is null, the word is added to the Datatable row
+                            /*******************************************************************/
+
+                            if (propertyToRetrieve == null || propertyToRetrieve.GetValue(objectOnList) == null)
                             {
-                                /*******************************************************************/
-                                // If the property value is null, the word is added to the Datatable row
-                                /*******************************************************************/
-
-                                if (propertyToRetrieve == null || propertyToRetrieve.GetValue(objectOnList) == null)
-                                {
-                                    listOfValuesForDataTableRow.Add("null");
-                                    continue;
-                                }
-
-                                /*******************************************************************/
-                                // If the property value is a list<String>, the list items are added
-                                // as comma separated values
-                                /*******************************************************************/
-
-                                if (propertyToRetrieve.PropertyType.ToString().Contains("System.Collections.Generic.List") &&
-                                propertyToRetrieve.GetValue(objectOnList).GetType().ToString().Contains("System.Collections.Generic.List`1[System.String]"))
-
-                                {
-                                    StringBuilder sb = new StringBuilder();
-
-                                    try
-                                    {
-                                        List<String> propertiesList = (List<String>)propertyToRetrieve.GetValue(objectOnList);
-
-                                        foreach (Object item in propertiesList)
-                                        {
-                                            sb.Append(item.ToString() + ",");
-                                        }
-                                    }
-                                    catch (Exception ex)
-                                    {
-                                        if (argShowDebug)
-                                        {
-                                            Console.WriteLine("Unable to cast object property to Datatable: " + ex.StackTrace);
-                                        }
-                                    }
-
-                                    listOfValuesForDataTableRow.Add(sb.ToString());
-                                }
-
-                                /*******************************************************************/
-                                // If the property is not a List<String>, just try to brute-force
-                                // add the value of the property
-                                /*******************************************************************/
-                                else
-                                {
-                                    listOfValuesForDataTableRow.Add(propertyToRetrieve.GetValue(objectOnList).ToString());
-                                }
+                                listOfValuesForDataTableRow.Add("null");
+                                continue;
                             }
-                            catch (Exception ex)
+
+                            /*******************************************************************/
+                            // If the property value is a list<String>, the list items are added
+                            // as comma separated values
+                            /*******************************************************************/
+
+                            if (propertyToRetrieve.PropertyType.ToString().Contains("System.Collections.Generic.List") &&
+                            propertyToRetrieve.GetValue(objectOnList).GetType().ToString().Contains("System.Collections.Generic.List`1[System.String]"))
+
                             {
-                                if (argShowDebug)
+                                StringBuilder sb = new StringBuilder();
+
+                                try
                                 {
-                                    Console.WriteLine("Unable to cast object properties to Datatable: " + ex.StackTrace);
+                                    List<String> propertiesList = (List<String>)propertyToRetrieve.GetValue(objectOnList);
+
+                                    foreach (Object item in propertiesList)
+                                    {
+                                        sb.Append(item.ToString() + ",");
+                                    }
                                 }
+                                catch (Exception ex)
+                                {
+                                    if (argShowDebug)
+                                    {
+                                        Console.WriteLine("Unable to cast object property to Datatable: " + ex.StackTrace);
+                                    }
+                                }
+
+                                listOfValuesForDataTableRow.Add(sb.ToString());
+                            }
+
+                            /*******************************************************************/
+                            // If the property is not a List<String>, just try to brute-force
+                            // add the value of the property
+                            /*******************************************************************/
+                            else
+                            {
+                                listOfValuesForDataTableRow.Add(propertyToRetrieve.GetValue(objectOnList).ToString());
                             }
                         }
-
-                        /*******************************************************************/
-                        // If the operation resulted on a useful row, add it to the
-                        // Datatable
-                        /*******************************************************************/
-
-                        if (listOfValuesForDataTableRow.Count > 0)
-                        {
-                            outputDataTable.Rows.Add(listOfValuesForDataTableRow.ToArray());
-                        }
-                        else
+                        catch (Exception ex)
                         {
                             if (argShowDebug)
                             {
-                                Console.WriteLine("Unable to cast object properties to Datatable. ");
+                                Console.WriteLine("Unable to cast object properties to Datatable: " + ex.StackTrace);
                             }
                         }
                     }
 
-                    return outputDataTable;
+                    /*******************************************************************/
+                    // If the operation resulted on a useful row, add it to the
+                    // Datatable
+                    /*******************************************************************/
+
+                    if (listOfValuesForDataTableRow.Count > 0)
+                    {
+                        outputDataTable.Rows.Add(listOfValuesForDataTableRow.ToArray());
+                    }
+                    else
+                    {
+                        if (argShowDebug)
+                        {
+                            Console.WriteLine("Unable to cast object properties to Datatable. ");
+                        }
+                    }
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("There was a problem while trying to show multiple objects as DataTable rows: " + ex.StackTrace, "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
-                    return new DataTable();
-                }
-            });
+
+                return outputDataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There was a problem while trying to show multiple objects as DataTable rows: " + ex.StackTrace, "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return new DataTable();
+            }
         }
     }
 }
